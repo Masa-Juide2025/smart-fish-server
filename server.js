70a491f726ab4933f9823beb54eafd007c911082
Masa-Juide2025 <s12112510@stu.najah.edu>
2025-08-25 13:16:45 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let sensorData = {};  // تخزين بيانات الحساسات مؤقتاً
+
+// استقبال البيانات من الESP
+app.post('/data', (req, res) => {
+  sensorData = req.body; // حفظ البيانات المرسلة
+  console.log('Received data:', sensorData);
+  res.sendStatus(200);
+});
+
+// إرسال البيانات لتطبيق الويب
+app.get('/data', (req, res) => {
+  res.json(sensorData);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
